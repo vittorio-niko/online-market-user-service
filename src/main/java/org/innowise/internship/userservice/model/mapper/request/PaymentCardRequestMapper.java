29e6e700b87c5149e57675f422c8d688019a22dc
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-01-23 00:54:10 +0300

    feat: add immutability for payment card properties

--- src/main/java/org/innowise/internship/userservice/model/mapper/request/PaymentCardRequestMapper.java
+++ src/main/java/org/innowise/internship/userservice/model/mapper/request/PaymentCardRequestMapper.java
@@ -2,18 +2,10 @@ package org.innowise.internship.userservice.model.mapper.request;
 
 import lombok.NonNull;
 import org.innowise.internship.userservice.model.dto.request.paymentcard.CreatePaymentCardRequestDto;
-import org.innowise.internship.userservice.model.dto.request.paymentcard.UpdatePaymentCardRequestDto;
 import org.innowise.internship.userservice.model.entity.PaymentCard;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface PaymentCardRequestMapper {
     PaymentCard toPaymentCard(@NonNull CreatePaymentCardRequestDto dto);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void updatePaymentCardFromDto(@NonNull UpdatePaymentCardRequestDto dto,
-                                  @MappingTarget PaymentCard card);
 }
