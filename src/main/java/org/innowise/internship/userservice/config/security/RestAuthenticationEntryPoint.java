770e4e0b76cd8402c65db4986b882c58463ae13e
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-03-08 00:02:59 +0300

    fix: unify error response form in security filter chain

--- src/main/java/org/innowise/internship/userservice/config/security/RestAuthenticationEntryPoint.java
+++ src/main/java/org/innowise/internship/userservice/config/security/RestAuthenticationEntryPoint.java
@@ -1,7 +1,11 @@
 package org.innowise.internship.userservice.config.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.innowise.internship.userservice.controller.exceptionhandler.ErrorCode;
+import org.innowise.internship.userservice.controller.exceptionhandler.ErrorResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,26 +13,25 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
             AuthenticationException authException
     ) throws IOException {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(ErrorCode.UNAUTHORIZED)
+                .message("Authentication is required to access this resource")
+                .build();
 
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json");
 
-        String json = """
-                {
-                  "status": 401,
-                  "error": "UNAUTHORIZED",
-                  "message": "Authentication is required to access this resource"
-                }
-                """;
-
-        response.getWriter().write(json);
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
 
