0d94333cca0455da4b883578b800b8d79b930cbc
vittorio-niko <viktor.nikonchik2007@gmail.com>
2026-04-06 14:49:34 +0300

    feat: configure external issuer-uri and internal jwk-set-uri to validate jwt properly in k8s profile

---
+++ src/main/java/org/innowise/internship/userservice/config/security/K8sSecurityConfig.java
@@ -0,0 +1,37 @@
+package org.innowise.internship.userservice.config.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@Profile("k8s")
+@Configuration
+public class K8sSecurityConfig {
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+
+        OAuth2TokenValidator<Jwt> issuerValidator = JwtValidators.createDefaultWithIssuer(issuerUri);
+
+        OAuth2TokenValidator<Jwt> combinedValidator = new DelegatingOAuth2TokenValidator<>(
+                JwtValidators.createDefault(),
+                issuerValidator
+        );
+
+        jwtDecoder.setJwtValidator(combinedValidator);
+        return jwtDecoder;
+    }
+}
